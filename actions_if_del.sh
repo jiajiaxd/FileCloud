755ba05be96b4d31426965cf3f03a0ce00163d53
甲甲 <51043917+jiajiaxd@users.noreply.github.com>
2022-03-20 17:09:47 +0800

    Update actions_if_del.sh

--- actions_if_del.sh
+++ actions_if_del.sh
@@ -1,7 +1,7 @@
 result=$(echo `git log -n 1 --summary` | grep delete)
 if [ -n "$result" ]; then
 	git log --diff-filter=D -n 1  --summary | grep 'delete' | tee changes/deletions.txt
-	cp changes/deletions.txt changes/AutoPatchWindows.bat
+	git log --diff-filter=D -n 1  --summary | grep 'delete' | tee changes/AutoPatchWindows.bat
 	sed -i "s/ delete mode 100644 //g" changes/deletions.txt
 	sed -i "s/ delete mode 100644 /del .minecraft\//g" changes/AutoPatchWindows.bat
 else
