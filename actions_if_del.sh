1b239bd26f13d42c561ad4126874822973a258ad
甲甲 <51043917+jiajiaxd@users.noreply.github.com>
2022-03-20 16:34:21 +0800

    Update actions_if_del.sh

--- actions_if_del.sh
+++ actions_if_del.sh
@@ -2,5 +2,5 @@ result=$(echo `git log -n 1 --summary` | grep delete)
 if [ -n "$result" ]; then
 	git log --diff-filter=D -n 1  --summary | grep 'delete' | tee changes/deletions.txt
 else
-	echo "::warning The latest commit has no deletions."
+	echo "The latest commit has no deletions."
 fi
