9e302749f39ec966e8d8d1b8cc47955f351b1855
甲甲 <51043917+jiajiaxd@users.noreply.github.com>
2022-03-21 11:31:08 +0800

    Update actions_if_del.sh

--- actions_if_del.sh
+++ actions_if_del.sh
@@ -1,4 +1,5 @@
-if  [ `git log -n 1 --summary` != `git log --diff-filter=D -n 1  --summary` ]; then
+result=$(echo `git log -n 1 --summary`  != `git log --diff-filter=D -n 1  --summary`)
+if [ -n "$result" ]; then
 	git diff-tree -r --no-commit-id --name-only `git rev-parse HEAD` | xargs -t -i{} cp --parents {} changes/
 fi
 result=$(echo `git log -n 1 --summary` | grep delete)
