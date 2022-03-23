1492becb399b019d46a3bd2f249d8eb820811fc8
甲甲 <51043917+jiajiaxd@users.noreply.github.com>
2022-03-23 13:30:46 +0800

    Update actions_if_del.sh

--- actions_if_del.sh
+++ actions_if_del.sh
@@ -5,9 +5,9 @@ fi
 result=$(echo `git log -n 1 --summary` | grep delete)
 if [ -n "$result" ]; then
 	git log --diff-filter=D -n 1  --summary | grep 'delete' | tee changes/deletions.txt
-	cp changes/deletions.txt changes/AutoPatchWindows.bat
+	cp changes/deletions.txt changes/AutoDeleteForWindows.bat
 	sed -i "s/ delete mode 100644 //g" changes/deletions.txt
-	sed -i "s/ delete mode 100644 /del .minecraft\//g" changes/AutoPatchWindows.bat
+	sed -i "s/ delete mode 100644 /del .minecraft\//g" changes/AutoDeleteForWindows.bat
 else
 	echo "The latest commit has no deletions."
 fi
